a6025507b7ff9ceee2efaea800de9fc7a6fd2d74
johan-st <jst@jst.dev>
2023-05-07 12:20:41 +0200

    update ignore and build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
+VERSION=$(git describe --tags --always --dirty)
 # Exit on error
 set -e
 
 # Build the project
 echo "Building the project..."
-go build -o bin/ ./...
-
-# Run the project
-echo "Running the project..."
-./bin/go-image-server
+go build -o bin/go-image-server_"$VERSION" .
 
 # Exit normally
 exit 0
