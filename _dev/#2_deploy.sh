ef5575b9c7fdcfaada58c9e7d954b3339de1fbd2
johan-st <jst@jst.dev>
2023-09-20 11:52:47 +0200

    edit scripts

--- _dev/#2_deploy.sh
+++ _dev/#2_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker tag img-server:latest registry.digitalocean.com/johan-st/img-server:latest
-docker tag img-server:latest registry.digitalocean.com/johan-st/img-server:"$(date +%Y%m%d_%H%M%S)"
-docker push -a registry.digitalocean.com/johan-st/img-server
+DATE_TAG=$(date +%Y%m%d_%H%M%S)
+LATEST_TAG="registry.digitalocean.com/johan-st/img-server:latest"
+
+docker tag img-server:latest "$LATEST_TAG"
+docker tag img-server:latest "registry.digitalocean.com/johan-st/img-server:$DATE_TAG"
+docker push "$LATEST_TAG"
+docker push "$DATE_TAG"
