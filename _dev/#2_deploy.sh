6f4b613bcd6df775bb0378f077073ebd0161115f
johan-st <jst@jst.dev>
2023-09-20 12:02:49 +0200

    fix deploy scripts

--- _dev/#2_deploy.sh
+++ _dev/#2_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DATE_TAG=$(date +%Y%m%d_%H%M%S)
+DATE_TAG="registry.digitalocean.com/johan-st/img-server:$(date +%Y%m%d_%H%M%S)"
 LATEST_TAG="registry.digitalocean.com/johan-st/img-server:latest"
 
 docker tag img-server:latest "$LATEST_TAG"
-docker tag img-server:latest "registry.digitalocean.com/johan-st/img-server:$DATE_TAG"
+docker tag img-server:latest "$DATE_TAG"
 docker push "$LATEST_TAG"
 docker push "$DATE_TAG"
